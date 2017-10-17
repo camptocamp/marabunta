ef44d55664897ca58354b03c897cf956c4ac4e7f
Denis Leemann <denis.leemann@camptocamp.com>
2017-10-17 13:35:01 +0200

    Add compatibility for python3

--- marabunta/model.py
+++ marabunta/model.py
@@ -208,7 +208,11 @@ class Operation(object):
 
     @staticmethod
     def _shlex_split_unicode(command):
-        return [l.decode('utf8') for l in shlex.split(command.encode('utf8'))]
+        if sys.version_info < (3, 4):
+            return [l.decode('utf8') for l in shlex.split(
+                command.encode('utf-8'))]
+        else:
+            return shlex.split(command)
 
     def __nonzero__(self):
         return bool(self.command)
