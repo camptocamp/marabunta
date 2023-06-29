ddbb2971d2c0bd068bd08266a41c02bb4fa52a20
Ivàn Todorovich <ivan.todorovich@gmail.com>
2023-06-29 12:54:11 -0300

    [FIX] flake8 errors
    ```
    | lint: commands[0]> flake8 marabunta/ tests/
    | tests/test_env_default.py:9:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:15:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:19:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:23:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:27:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:31:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:35:11: E275 missing whitespace after keyword
    | tests/test_env_default.py:39:11: E275 missing whitespace after keyword
    ```

--- tests/test_env_default.py
+++ tests/test_env_default.py
@@ -6,34 +6,34 @@ import os
 def test_env_default_str():
     os.environ['test_var'] = 'Foo'
     test = EnvDefault('test_var', option_strings='', dest='test_var')
-    assert(test.default == 'Foo')
+    assert test.default == 'Foo'
 
 
 def test_bool_env_default_str():
     os.environ['test_var'] = 'True'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(test.default)
+    assert test.default
 
     os.environ['test_var'] = 'true'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(test.default)
+    assert test.default
 
     os.environ['test_var'] = '1'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(test.default)
+    assert test.default
 
     os.environ['test_var'] = 'False'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(not test.default)
+    assert not test.default
 
     os.environ['test_var'] = 'false'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(not test.default)
+    assert not test.default
 
     os.environ['test_var'] = '3'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(not test.default)
+    assert not test.default
 
     del os.environ['test_var']
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
-    assert(not test.default)
+    assert not test.default
