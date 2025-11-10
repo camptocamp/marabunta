fe0a1c8ad5434019c4e273d6e0a61302a1bf07bb
Iván Todorovich <ivan.todorovich@gmail.com>
2025-11-10 09:34:53 -0300

    [IMP] Fallback to common PG env vars

--- tests/test_env_default.py
+++ tests/test_env_default.py
@@ -9,6 +9,31 @@ def test_env_default_str():
     assert test.default == 'Foo'
 
 
+def test_env_default_str_multiple():
+    os.environ['test_var_2'] = 'Foo'
+    test = EnvDefault(('test_var', 'test_var_2'), option_strings='', dest='test_var')
+    assert test.default == 'Foo'
+
+
+def test_env_default_with_regular_default():
+    # Env var is set, so it's used instead of the regular default
+    os.environ['test_var_3'] = 'Foo'
+    test = EnvDefault('test_var', option_strings='', dest='test_var', default='Bar')
+    assert test.default == 'Foo'
+
+
+def test_env_default_with_regular_default_and_no_env_var():
+    # No env var set, so the regular default should be used
+    test = EnvDefault('test_var_4', option_strings='', dest='test_var', default='Bar')
+    assert test.default == 'Bar'
+
+
+def test_env_default_with_regular_default_and_no_env_var_and_no_regular_default():
+    # No env var set, no regular default, so it should be None
+    test = EnvDefault('test_var_5', option_strings='', dest='test_var')
+    assert test.default is None
+
+
 def test_bool_env_default_str():
     os.environ['test_var'] = 'True'
     test = BoolEnvDefault('test_var', option_strings='', dest='test_var')
